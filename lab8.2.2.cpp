c6e73d25f55fc479b7b501ea90bac81a8ac51ece
KazakovOlexanderIKM723a <163044623+KazakovOlexanderIKM723a@users.noreply.github.com>
2024-03-18 13:55:39 +0200

    Add files via upload

---
+++ lab8.2.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int arraySize = 10;
+    double array[arraySize];
+
+    cout << "Enter " << arraySize << " real numbers for the array:" << endl;
+    for (int i = 0; i < arraySize; ++i) {
+        cin >> array[i];
+    }
+
+    double sum = 0.0;
+    for (int i = 0; i < arraySize; ++i) {
+        array[i] += sum;
+        sum += array[i];
+    }
+
+    cout << "Changed array:\n ";
+    for (int i = 0; i < arraySize; ++i) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
